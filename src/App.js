51afab5db644e31bd94c67ca4cbb6f932ed4c341
Ally Elvis Nzeyimana <allyelvis6569@gmail.com>
2025-05-23 17:09:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NFTUploader from "./components/NFTUploader";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen bg-gray-100 p-6 font-sans">
+        <h1 className="text-3xl font-bold text-center mb-8">Aenzbi Metaverse NFT Generator</h1>
+        <Routes>
+          <Route path="/" element={<NFTUploader />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
